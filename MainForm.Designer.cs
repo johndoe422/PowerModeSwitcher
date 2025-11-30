8fb7f725b08d09470cb839db9a542402b6fcf56c
johndoe422 <30309651+johndoe422@users.noreply.github.com>
2025-11-30 18:10:02 +0530

    Added UI for power mode auto switch

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -36,7 +36,6 @@
             this.tslblCPUSpeed = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
             this.tslblRunningAvg = new System.Windows.Forms.ToolStripStatusLabel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
@@ -44,9 +43,15 @@
             this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.checkBoxCpuSpeed = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbWhenInUse = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbWhenIdle = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chkboxEnableAutoSwitch = new System.Windows.Forms.CheckBox();
             this.statusStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.contextMenuStrip.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // statusStrip1
@@ -58,9 +63,9 @@
             this.tslblCPUSpeed,
             this.toolStripStatusLabel2,
             this.tslblRunningAvg});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 75);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 191);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(442, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(429, 22);
             this.statusStrip1.TabIndex = 0;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -94,31 +99,22 @@
             this.tslblRunningAvg.Size = new System.Drawing.Size(150, 17);
             this.tslblRunningAvg.Text = "1 Min Average: 2.34 Ghz";
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::PowerModes.Properties.Resources.Power202;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(48, 48);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(70, 25);
+            this.label1.Location = new System.Drawing.Point(12, 19);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.Size = new System.Drawing.Size(99, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Active Power Plan:";
+            this.label1.Text = "Current power plan:";
             // 
             // comboBox1
             // 
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(169, 21);
+            this.comboBox1.Location = new System.Drawing.Point(117, 16);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(261, 21);
+            this.comboBox1.Size = new System.Drawing.Size(289, 21);
             this.comboBox1.TabIndex = 3;
             // 
             // notifyIcon
@@ -153,23 +149,83 @@
             // checkBoxCpuSpeed
             // 
             this.checkBoxCpuSpeed.AutoSize = true;
-            this.checkBoxCpuSpeed.Location = new System.Drawing.Point(309, 53);
+            this.checkBoxCpuSpeed.Location = new System.Drawing.Point(285, 167);
             this.checkBoxCpuSpeed.Name = "checkBoxCpuSpeed";
             this.checkBoxCpuSpeed.Size = new System.Drawing.Size(121, 17);
             this.checkBoxCpuSpeed.TabIndex = 4;
             this.checkBoxCpuSpeed.Text = "CPU Speed Overlay";
             this.checkBoxCpuSpeed.UseVisualStyleBackColor = true;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "While in use: ";
+            // 
+            // cbWhenInUse
+            // 
+            this.cbWhenInUse.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbWhenInUse.FormattingEnabled = true;
+            this.cbWhenInUse.Location = new System.Drawing.Point(95, 46);
+            this.cbWhenInUse.Name = "cbWhenInUse";
+            this.cbWhenInUse.Size = new System.Drawing.Size(296, 21);
+            this.cbWhenInUse.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "When idle:";
+            // 
+            // cbWhenIdle
+            // 
+            this.cbWhenIdle.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbWhenIdle.FormattingEnabled = true;
+            this.cbWhenIdle.Location = new System.Drawing.Point(95, 73);
+            this.cbWhenIdle.Name = "cbWhenIdle";
+            this.cbWhenIdle.Size = new System.Drawing.Size(296, 21);
+            this.cbWhenIdle.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chkboxEnableAutoSwitch);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cbWhenIdle);
+            this.groupBox1.Controls.Add(this.cbWhenInUse);
+            this.groupBox1.Location = new System.Drawing.Point(15, 55);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(402, 106);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Power Plan Auto Switch";
+            // 
+            // chkboxEnableAutoSwitch
+            // 
+            this.chkboxEnableAutoSwitch.AutoSize = true;
+            this.chkboxEnableAutoSwitch.Location = new System.Drawing.Point(95, 22);
+            this.chkboxEnableAutoSwitch.Name = "chkboxEnableAutoSwitch";
+            this.chkboxEnableAutoSwitch.Size = new System.Drawing.Size(119, 17);
+            this.chkboxEnableAutoSwitch.TabIndex = 4;
+            this.chkboxEnableAutoSwitch.Text = "Enable Auto Switch";
+            this.chkboxEnableAutoSwitch.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(442, 97);
+            this.ClientSize = new System.Drawing.Size(429, 213);
             this.Controls.Add(this.checkBoxCpuSpeed);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -179,8 +235,9 @@
             this.TopMost = true;
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.contextMenuStrip.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -190,7 +247,6 @@
 
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
@@ -202,6 +258,12 @@
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
         private System.Windows.Forms.ToolStripStatusLabel tslblRunningAvg;
         private System.Windows.Forms.CheckBox checkBoxCpuSpeed;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbWhenInUse;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbWhenIdle;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox chkboxEnableAutoSwitch;
     }
 }
 
