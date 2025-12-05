49ae5b7d64d9c477af97f2263dd393b967078c06
johndoe422 <30309651+johndoe422@users.noreply.github.com>
2025-12-05 22:47:31 +0530

    Added option "Show notifications" , only UI

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -62,6 +62,7 @@
             this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.statusStrip1.SuspendLayout();
             this.contextMenuStrip.SuspendLayout();
             this.groupBox1.SuspendLayout();
@@ -121,7 +122,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Location = new System.Drawing.Point(12, 22);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(99, 13);
             this.label1.TabIndex = 2;
@@ -131,9 +132,9 @@
             // 
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(117, 16);
+            this.comboBox1.Location = new System.Drawing.Point(117, 19);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(289, 21);
+            this.comboBox1.Size = new System.Drawing.Size(300, 21);
             this.comboBox1.TabIndex = 3;
             // 
             // notifyIcon
@@ -179,7 +180,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(24, 197);
+            this.label2.Location = new System.Drawing.Point(24, 200);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(71, 13);
             this.label2.TabIndex = 2;
@@ -189,7 +190,7 @@
             // 
             this.cbWhenInUse.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbWhenInUse.FormattingEnabled = true;
-            this.cbWhenInUse.Location = new System.Drawing.Point(95, 194);
+            this.cbWhenInUse.Location = new System.Drawing.Point(95, 197);
             this.cbWhenInUse.Name = "cbWhenInUse";
             this.cbWhenInUse.Size = new System.Drawing.Size(296, 21);
             this.cbWhenInUse.TabIndex = 3;
@@ -197,7 +198,7 @@
             // lblOnIdle
             // 
             this.lblOnIdle.AutoSize = true;
-            this.lblOnIdle.Location = new System.Drawing.Point(12, 224);
+            this.lblOnIdle.Location = new System.Drawing.Point(12, 227);
             this.lblOnIdle.Name = "lblOnIdle";
             this.lblOnIdle.Size = new System.Drawing.Size(79, 13);
             this.lblOnIdle.TabIndex = 2;
@@ -207,7 +208,7 @@
             // 
             this.cbWhenIdle.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbWhenIdle.FormattingEnabled = true;
-            this.cbWhenIdle.Location = new System.Drawing.Point(95, 221);
+            this.cbWhenIdle.Location = new System.Drawing.Point(95, 224);
             this.cbWhenIdle.Name = "cbWhenIdle";
             this.cbWhenIdle.Size = new System.Drawing.Size(296, 21);
             this.cbWhenIdle.TabIndex = 3;
@@ -239,7 +240,7 @@
             // lblOnIdle2
             // 
             this.lblOnIdle2.AutoSize = true;
-            this.lblOnIdle2.Location = new System.Drawing.Point(9, 119);
+            this.lblOnIdle2.Location = new System.Drawing.Point(9, 122);
             this.lblOnIdle2.Name = "lblOnIdle2";
             this.lblOnIdle2.Size = new System.Drawing.Size(79, 13);
             this.lblOnIdle2.TabIndex = 7;
@@ -248,7 +249,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(21, 92);
+            this.label5.Location = new System.Drawing.Point(21, 95);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(71, 13);
             this.label5.TabIndex = 8;
@@ -258,7 +259,7 @@
             // 
             this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(92, 116);
+            this.comboBox2.Location = new System.Drawing.Point(92, 119);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(296, 21);
             this.comboBox2.TabIndex = 9;
@@ -267,7 +268,7 @@
             // 
             this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(92, 89);
+            this.comboBox3.Location = new System.Drawing.Point(92, 92);
             this.comboBox3.Name = "comboBox3";
             this.comboBox3.Size = new System.Drawing.Size(296, 21);
             this.comboBox3.TabIndex = 10;
@@ -330,7 +331,7 @@
             this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox3.Location = new System.Drawing.Point(6, 66);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(390, 88);
+            this.groupBox3.Size = new System.Drawing.Size(390, 92);
             this.groupBox3.TabIndex = 11;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "On Battery";
@@ -348,7 +349,7 @@
             // 
             this.groupBox4.Controls.Add(this.pictureBox2);
             this.groupBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox4.Location = new System.Drawing.Point(6, 168);
+            this.groupBox4.Location = new System.Drawing.Point(6, 171);
             this.groupBox4.Name = "groupBox4";
             this.groupBox4.Size = new System.Drawing.Size(390, 91);
             this.groupBox4.TabIndex = 12;
@@ -366,6 +367,7 @@
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.checkBox1);
             this.groupBox2.Controls.Add(this.checkBoxCpuSpeed);
             this.groupBox2.Location = new System.Drawing.Point(15, 52);
             this.groupBox2.Name = "groupBox2";
@@ -373,6 +375,19 @@
             this.groupBox2.TabIndex = 6;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "General Settings";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.checkBox1.Location = new System.Drawing.Point(243, 19);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(120, 18);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Show Notifications";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
@@ -441,6 +456,7 @@
         private System.Windows.Forms.GroupBox groupBox4;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
